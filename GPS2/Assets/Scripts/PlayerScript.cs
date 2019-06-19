b984091ab44c34656a203f553446ff9b363c2516
Doolele <senpaicheng@hotmail.com>
2019-06-19 14:53:33 +0800

    Built the level and checkpoints

--- GPS2/Assets/Scripts/PlayerScript.cs
+++ GPS2/Assets/Scripts/PlayerScript.cs
@@ -1,37 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-   // Vector3 lastPos;
-   
-    // Start is called before the first frame update
+    public Transform cP1;
+    public Transform cP2;
+    public Transform cP3;
+
+    private bool IsCP1Checked;
+    private bool IsCP2Checked;
+    private bool IsCP3Checked;
+
+
     void Start()
     {
-        //GameManager.Instance.player = this.gameObject;
-       
+        IsCP1Checked = false;
+        IsCP2Checked = false;
+        IsCP3Checked = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-       
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag.Equals("Enemy"))
+        if (collision.gameObject.tag.Equals("Hazard"))
         {
-            Destroy(this.gameObject);
-            //SceneManager.LoadScene("LosingScene");
+            //Destroy(this.gameObject);
+            if(IsCP1Checked)
+            {
+                this.gameObject.transform.position = cP1.transform.position;
+            }
+            else if(IsCP1Checked && IsCP2Checked)
+            {
+                this.gameObject.transform.position = cP2.transform.position;
+            }
+            else if (IsCP1Checked && IsCP2Checked && IsCP3Checked)
+            {
+                this.gameObject.transform.position = cP3.transform.position;
+            }
         }
         else if (collision.gameObject.tag.Equals("Exit"))
         {
-            //SceneManager.LoadScene("WinningScene");
+            SceneManager.LoadScene("POC");
         }
     }
 
-
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Checkpoint1")
+        {
+            IsCP1Checked = true;
+        }
+        else if (collider.gameObject.tag == "Checkpoint2")
+        {
+            IsCP2Checked = true;
+        }
+        else if (collider.gameObject.tag == "Checkpoint3")
+        {
+            IsCP3Checked = true;
+        }
+    }
 }
