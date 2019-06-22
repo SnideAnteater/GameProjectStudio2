f1665fd8620b58040172471c070f51b453f8bebb
TYHong97 <tyhong1997@gmail.com>
2019-06-22 21:37:54 +0800

    Normalized the values (someone pls adjust the values as the sensitivity is ultra low now)

--- GPS2/Assets/Scripts/Rotation.cs
+++ GPS2/Assets/Scripts/Rotation.cs
@@ -22,13 +22,13 @@ public class Rotation : MonoBehaviour
     void Update()
     {
 
-        /*curRotation = this.transform.rotation.z;
+        curRotation = this.transform.rotation.z;
         if(Input.touchCount>0)
         {
             Touch touch = Input.GetTouch(0);
 
             if (touch.position.x > Screen.width / 2)
-                this.transform.Rotate(0, 0, touch.deltaPosition.y*Time.deltaTime*sensitivity);//TRY ADD TORQUE OR ADD FORCE
+                this.transform.Rotate(0, 0, touch.deltaPosition.normalized.y*Time.deltaTime*sensitivity);//TRY ADD TORQUE OR ADD FORCE
                 
              // rb.AddTorque(0, 0, touch.deltaPosition.y * Time.deltaTime * sensitivity);
 
@@ -36,11 +36,11 @@ public class Rotation : MonoBehaviour
                 //  rb.AddTorque(0, 0, touch.deltaPosition.y * Time.deltaTime * sensitivity);
               
             
-            this.transform.Rotate(0, 0, -(touch.deltaPosition.y*Time.deltaTime*sensitivity));
+            this.transform.Rotate(0, 0, -(touch.deltaPosition.normalized.y * Time.deltaTime*sensitivity));
            
-        }*/ //add torque or transform rotate
+        } //add torque or transform rotate
 
-        if (Input.touchCount > 0) // move rotation
+       /* if (Input.touchCount > 0) // move rotation
         {
             Touch touch = Input.GetTouch(0);
 
@@ -61,7 +61,7 @@ public class Rotation : MonoBehaviour
             rb.MoveRotation(rb.rotation * deltaRotation);
 
 
-        }
+        }*/
        
     }
 
