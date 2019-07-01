facd486cd9d9b6e48291672af2102fbb5ebc47dc
Doolele <senpaicheng@hotmail.com>
2019-07-01 15:01:52 +0800

    Pause Menu and Main Menu but the animation for the buttons in main menu is not working when level reloaded

---
+++ GPS2/Assets/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingUI;
+    public AudioSource buttonSoundEffect;
+    public AudioSource music;
+
+
+    void Start()
+    {
+        music.volume = PlayerPrefs.GetFloat("MusicVolume");
+        settingUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        buttonSoundEffect.volume = PlayerPrefs.GetFloat("FxVolume");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SettingMenu()
+    {
+        settingUI.SetActive(true);
+    }
+
+    public void Back()
+    {
+        settingUI.SetActive(false);
+    }
+}
+
