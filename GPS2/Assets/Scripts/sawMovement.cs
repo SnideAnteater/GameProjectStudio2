18de2e0fe6286cdd98fb95a89d008b3df9102655
TYHong97 <tyhong1997@gmail.com>
2019-07-08 18:39:49 +0800

    updated saw and doors

--- GPS2/Assets/Scripts/sawMovement.cs
+++ GPS2/Assets/Scripts/sawMovement.cs
@@ -6,22 +6,29 @@ public class sawMovement : MonoBehaviour
 {
     public float speed = 5;
     public GameObject[] checkpoints = new GameObject[5];
-    private int currentPoint =0 ;
+    private int currentPoint = 0;
     private Vector3 target;
     private bool ascending = true;
+
+    public enum moveType {reversing=0, looping=1};
+    public moveType type = moveType.reversing;
     // Start is called before the first frame update
     void Start()
     {
-        target = checkpoints[0].transform.position;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        target = checkpoints[currentPoint].transform.position;
         this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(target.x, target.y,this.transform.position.z), Time.deltaTime * speed);
-        if(Vector2.Distance(this.transform.position,target) == 0)
+        if(Vector2.Distance(this.transform.position,target) <= 0.1)
         {
-            ChangePoint();
+            if (type == moveType.reversing)
+                ChangePoint();
+            else
+                ChangePointLoop();
         }
     }
 
@@ -40,7 +47,21 @@ public class sawMovement : MonoBehaviour
             ascending = false;
         }
 
-        target = checkpoints[currentPoint].transform.position;
+        //target = checkpoints[currentPoint].transform.position;
+
+    }
+
+    void ChangePointLoop()
+    {
+
+        currentPoint++;
+        if (currentPoint >= checkpoints.Length )
+        {
+            currentPoint = 0;
+        }
+        
+
+        //target = checkpoints[currentPoint].transform.position;
 
     }
 }
