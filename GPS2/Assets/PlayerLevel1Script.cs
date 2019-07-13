266968c7ddf521b42637505141a05a59429a39ed
Doolele <senpaicheng@hotmail.com>
2019-07-13 10:18:36 +0800

    no message

--- GPS2/Assets/PlayerLevel1Script.cs
+++ GPS2/Assets/PlayerLevel1Script.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerLevel1Script : MonoBehaviour
 {
     public Transform cP1;
     public Transform cP2;
     public Transform cP3;
-    public Transform Chase1;
-    public Transform Chase2;
+
+    public GameObject cP1Wall;
+    public GameObject cP2Wall;
+    public GameObject cP3Wall;
 
     private bool IsCP1Checked;
     private bool IsCP2Checked;
@@ -18,18 +20,24 @@ public class PlayerScript : MonoBehaviour
 
     void Start()
     {
+
+        cP1Wall.SetActive(false);
+        cP2Wall.SetActive(false);
+        cP3Wall.SetActive(false);
         IsCP1Checked = false;
         IsCP2Checked = false;
         IsCP3Checked = false;
 
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Hazard")
+        if (other.gameObject.tag == "Hazard")
         {
             Dead();
         }
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Hazard")
@@ -38,7 +46,7 @@ public class PlayerScript : MonoBehaviour
         }
         else if (collision.gameObject.tag == "Exit")
         {
-            SceneManager.LoadScene("POC");
+            SceneManager.LoadScene("Level1");
         }
     }
 
@@ -46,45 +54,40 @@ public class PlayerScript : MonoBehaviour
     {
         if (collider.gameObject.tag == "Checkpoint1")
         {
+            cP1Wall.SetActive(true);
             Debug.Log("CP1");
             IsCP1Checked = true;
         }
         else if (collider.gameObject.tag == "Checkpoint2")
         {
+            cP2Wall.SetActive(true);
             Debug.Log("CP2");
-            Chase1.gameObject.SetActive(true);
             IsCP2Checked = true;
         }
         else if (collider.gameObject.tag == "Checkpoint3")
         {
+            cP3Wall.SetActive(true);
             Debug.Log("CP3");
-            Chase2.gameObject.SetActive(true);
             IsCP3Checked = true;
         }
     }
 
     private void Dead()
     {
-            Debug.Log("Dead");
-            //Destroy(this.gameObject);
-            if (IsCP1Checked && !IsCP2Checked && !IsCP3Checked)
-            {
-                this.gameObject.transform.position = cP1.transform.position;
-            }
-            else if (IsCP1Checked && IsCP2Checked && !IsCP3Checked)
-            {
-                Chase1.gameObject.SetActive(false);
-                this.gameObject.transform.position = cP2.transform.position;
-            }
-            else if (IsCP1Checked && IsCP2Checked && IsCP3Checked)
-            {
-                Chase2.gameObject.SetActive(false);
-                this.gameObject.transform.position = cP3.transform.position;
-            }
-            else
-            {
-                SceneManager.LoadScene("POC");
-            }
-        
+        Debug.Log("Dead");
+        //Destroy(this.gameObject);
+        if (IsCP1Checked && !IsCP2Checked && !IsCP3Checked)
+        {
+            this.gameObject.transform.position = cP1.transform.position;
+        }
+        else if (IsCP1Checked && IsCP2Checked && !IsCP3Checked)
+        {
+            this.gameObject.transform.position = cP2.transform.position;
+        }
+        else if (IsCP1Checked && IsCP2Checked && IsCP3Checked)
+        {
+            this.gameObject.transform.position = cP3.transform.position;
+        }
     }
 }
+
