9d471ec7defd3f069233c9e1af6f155dddaf51bb
TYHong97 <tyhong1997@gmail.com>
2019-07-13 19:01:12 +0800

    Deleted failed spike and added checkpoint locking door

---
+++ GPS2/Assets/CheckpointDoor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointDoor : MonoBehaviour
+{
+    public GameObject doorDestination;
+    public GameObject closePos;
+    private bool triggered = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player" && triggered == false)
+        {
+            doorDestination.transform.position = new Vector3(closePos.transform.position.x, closePos.transform.position.y, closePos.transform.position.z);
+        }
+    }
+}
---
+++ GPS2/Assets/DoorPosSync.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPosSync : MonoBehaviour
+{
+    public GameObject door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = door.transform.position;
+    }
+
+}
